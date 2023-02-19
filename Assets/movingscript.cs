35b455afdee8fbfed35c5b672425f87906432be8
ivalchanka <i.valchenk@hotmail.com>
2023-02-19 10:24:08 +0400

    Added project skeleton.

---
+++ Assets/movingscript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class movingscript : MonoBehaviour
+{
+    public float speedacelerator = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x - speedacelerator, this.transform.position.y, this.transform.position.z);
+    }
+}
